877d07d8659c03143a1f11b40f6e5a2840805205
bonjinnorenka <32708102+bonjinnorenka@users.noreply.github.com>
2025-06-16 12:33:06 +0000

    Revert "v3.0.0 convert to typescript"
    This reverts commit 2d12e76fc6930d66dfea9bac5bb11225ca7f9a11.

--- iframe_api/index.ts
+++
@@ -1,8 +0,0 @@
-// Export all player classes
-export { mep_youtube } from './youtube';
-export { mep_bilibili } from './bilibili';
-export { mep_soundcloud } from './soundcloud';
-export { mep_niconico } from './niconico';
-
-// Export types
-export * from '../types'; 
